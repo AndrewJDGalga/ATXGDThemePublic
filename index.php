79d1eff24f17cc534abb6b789c67eef229e58a9b
D.Galga <70071943+AndrewJDGalga@users.noreply.github.com>
2023-07-29 10:30:17 -0500

    Update version 2.2
    Uploading for professor to review if needed.

--- index.php
+++ index.php
@@ -2,30 +2,34 @@
 <html>
     <head>
         <meta charset="<?php bloginfo( 'charset' ); ?>">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
         <title><?php wp_title('|', true, 'right'); ?></title>
         <link rel="stylesheet" href="<?php echo esc_url(get_stylesheet_uri()); ?>" type="text/css">
         <!--Adobe Hightower + Bilo font link-->
         <link rel="stylesheet" href="https://use.typekit.net/fgr8upo.css">
     </head>
     <body>
-        <header class="header-main">
-            <img class="header-backing-img" src="<?php echo get_template_directory_uri() . '/assets/banner/ATX-GD-Web-banner-photo-onlyCrop1.webp' ?>" alt="Different colored meeples (small wooden figures) and boardgame components fill a table.">
-            <div class="logo-nav-wrapper">
-                <img class="header-group-name" src="<?php echo get_template_directory_uri() . '/assets/banner/ATX-GD-Logo-CW-02.webp' ?>" alt="ATX Game Designers Group name">
-                <?php 
-                    //dynamic menu
-                    wp_nav_menu(array(
-                        'theme_location' => 'primary-menu',
-                        'container' => '',
-                        'menu_class' => 'wp-nav-ul'
-                    ));
-                ?>
-            </div>
+        <header class="atxgd-header">
+                <div class="header_logo header_backing-image">
+                    <a class="header_home-wrapper" href="<?php echo home_url( ); ?>">
+                        <img src="<?php echo get_template_directory_uri() . '/assets/banner/ATX-GD-Logo-CW-02.webp' ?>" alt="ATX Game Designers Group name">
+                    </a>
+                </div>
+                <div class="header_nav">
+                    <?php 
+                        //dynamic menu
+                        wp_nav_menu(array(
+                            'theme_location' => 'primary-menu',
+                            'container' => '',
+                            'menu_class' => 'wp-nav-ul'
+                        ));
+                    ?>
+                </div>
         </header>
 
         <?php wp_head(); ?>
 
-        <article>
+        <article class="atxgd-content">
             <?php if(have_posts(  )) : while(have_posts(  )) : the_post(  );?>
                 <h1 style="display: none;"><?php the_title( ); ?></h1>
                 <?php the_content(); ?>
@@ -44,16 +48,15 @@
         </article>
 
         <?php wp_footer(); ?>
-        <footer class="theme-footer">
-            <div class="high-center-wave">
-                <div class="icon-container">
-                    <a href="https://www.facebook.com/groups/austinboardgamedesigners" target="_blank">
-                        <img class="facebook-svg" src="<?php echo get_template_directory_uri() . '/assets/icons/icons8-facebook_green.svg' ?>" alt="Green-colored SVG of the Facebook F logo">
-                    </a>
-                    <a href="https://www.meetup.com/austin-board-game-designers-and-play-testers/" target="_blank">
-                        <img class="meetup-svg" src="<?php echo get_template_directory_uri() . '/assets/icons/meetup-svgrepo-com.svg' ?>" alt="Green-colored SVG of the Meetup 'Swarm' logo">
-                    </a>
-                </div>
+
+        <footer class="atxgd-footer">
+            <div class="icon-container high-center-wave">
+                <a href="https://www.facebook.com/groups/austinboardgamedesigners" target="_blank">
+                    <img class="facebook-svg" src="<?php echo get_template_directory_uri() . '/assets/icons/icons8-facebook_green.svg' ?>" alt="Green-colored SVG of the Facebook F logo">
+                </a>
+                <a href="https://www.meetup.com/austin-board-game-designers-and-play-testers/" target="_blank">
+                    <img class="meetup-svg" src="<?php echo get_template_directory_uri() . '/assets/icons/meetup-svgrepo-com.svg' ?>" alt="Green-colored SVG of the Meetup 'Swarm' logo">
+                </a>
             </div>
         </footer>
     </body>
